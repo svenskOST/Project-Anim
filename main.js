49d13865fd9e3b00bf3bb8708b72885c2b5f8b20
Alexander Marini <svenskost6@gmail.com>
2022-11-01 19:21:09 +0100

    svg almost complete

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-var logo = document.getElementById("logo")
 var loading = document.getElementById("loading")
+var svg = document.getElementsByTagName("svg")[0]
+var originalsvg = svg.getBoundingClientRect()
+
 setTimeout(function () {
-    logo.style.left = "10px"
-    logo.style.top = "10px"
+    //logo.style.left = "100px"
+    svg.setAttribute("width", originalsvg.width * 0.5 + "px")
+    svg.setAttribute("height", originalsvg.height * 0.5 + "px")
+    svg.setAttribute("stroke-width", 6)
     setTimeout(function () {
         loading.style.opacity = 0
         setTimeout(function () {
