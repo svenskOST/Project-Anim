3f3c0050c0d402e142927e9786cb35615955c9d1
Alexander Marini <svenskost6@gmail.com>
2022-11-01 02:09:38 +0100

    svg created

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-var startup = document.getElementsByClassName("startup")
+var logo = document.getElementById("logo")
+var loading = document.getElementById("loading")
 setTimeout(function () {
-    startup.style.opacity = 0
+    logo.style.left = "10px"
+    logo.style.top = "10px"
     setTimeout(function () {
-        startup.style.display = "none"
-    }, 1000)
-}, 4000)
+        loading.style.opacity = 0
+        setTimeout(function () {
+            loading.style.display = "none"
+        }, 1000)
+    }, 3000)
+}, 6000)
---
+++ themes.js
@@ -0,0 +1,17 @@
+var theme = document.getElementById("themeselector")
+
+if(theme == "standard"){
+
+}
+
+if(theme == "matteblack"){
+
+}
+
+if(theme == "neon"){
+
+}
+
+if(theme == "rgb"){
+
+}
