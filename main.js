89f7f62ea5da1f6de2ca45244f5a707712c2fdd5
Alexander Marini <svenskost6@gmail.com>
2022-11-02 23:13:10 +0100

    svg complete + bug fixes

--- main.js
+++ main.js
@@ -2,17 +2,28 @@ var loading = document.getElementById("loading")
 var svg = document.getElementsByTagName("svg")[0]
 var originalsvg = svg.getBoundingClientRect()
 
+window.scrollTo(0, 0)
+window.onscroll = function() {
+    window.scrollTo(0, 0)
+}
+
 setTimeout(function () {
-    svg.style.position = "absolute"
+    svg.style.transition = "3s ease-in-out"
+    svg.style.transform = "translate(0, 0)"
     svg.style.left = "30px"
     svg.style.top = "30px"
     svg.setAttribute("width", originalsvg.width * 0.6 + "px")
     svg.setAttribute("height", originalsvg.height * 0.6 + "px")
     svg.setAttribute("stroke-width", 5)
     setTimeout(function () {
+        svg.style.transition = "0"
         loading.style.opacity = 0
         setTimeout(function () {
             loading.style.display = "none"
+            window.onscroll = function() {}
         }, 1000)
     }, 3000)
 }, 6000)
+
+//göra så att scrollbar inte syns över loading screen
+//lägga till loading animation (såndär hjulgrej)
--- themes.js
+++ themes.js
@@ -1,17 +1,25 @@
 var theme = document.getElementById("themeselector")
+var svg = document.getElementsByTagName("svg")[0]
+var scrollbar = document.getElementsByName("::-webkit-scrollbar")
 
 if(theme == "standard"){
-
+    svg.setAttribute("stroke", "purple")
+    scrollbar.style.background = "purple"
 }
 
 if(theme == "matteblack"){
-
+    svg.setAttribute("stroke", "black")
+    scrollbar.style.background = "black"
 }
 
 if(theme == "neon"){
-
+    svg.setAttribute("stroke", temp)
+    scrollbar.style.background = temp
 }
 
 if(theme == "rgb"){
-
+    svg.setAttribute("stroke", temp)
+    scrollbar.style.background = temp
 }
+
+//scrollbar funkar inte än, bara som placeholder (vet inte hur jag kan accessa ::-webkit-scrollbar)
