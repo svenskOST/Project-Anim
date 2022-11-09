47f412eda998d377b300c5381b632de59179dce9
Alexander Marini <svenskost6@gmail.com>
2022-11-09 22:48:02 +0100

    scrollbar drag progress

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 var scrollbar = document.getElementById("scrollbar")
 var loading = document.getElementById("loading")
+var background = document.getElementById("background")
 var content = document.getElementById("content")
 var logo = document.getElementById("logo")
 var originallogo = logo.getBoundingClientRect()
 var anim1 = document.getElementById("anim1")
-
-window.scrollTo(0, 0)
-window.onscroll = function() {
-    window.scrollTo(0, 0)
-}
 
 setTimeout(function () {
     logo.style.transition = "2s ease-in-out"
@@ -18,21 +14,34 @@ setTimeout(function () {
     logo.setAttribute("width", originallogo.width * 0.6 + "px")
     logo.setAttribute("height", originallogo.height * 0.6 + "px")
     logo.setAttribute("stroke-width", 5)
+
     setTimeout(function () {
         logo.style.transition = 0
         loading.style.opacity = 0
         scrollbar.style.transitionDuration = "1s"
-            scrollbar.style.height = ( window.innerHeight / (anim1.clientHeight * 4 + 130) * 100 + "%")
-            setTimeout(function () {
-                scrollbar.style.transitionDuration = "0s" 
-            }, 1000)
+        scrollbar.style.height = (window.innerHeight / (anim1.clientHeight * 4 + 130) * 100 + "%")
+       
         setTimeout(function () {
+            scrollbar.style.transitionDuration = "0s"
             loading.style.display = "none"
-            window.onscroll = function() {}
             content.appendChild(logo)
+            
+            background.onscroll = function() {
+                var scrollfactor = background.scrollTop / (anim1.clientHeight * 4 + 130)
+                scrollbar.style.top = window.innerHeight * scrollfactor + "px"
+            }
+            scrollbar.onmouseenter = scrolldrag
+            function scrolldrag() {
+                document.onmousedown = function() {
+                    console.log("hi")
+                    var scrollcurrent = background.scrollTop
+                    let x = scrollcurrent // +/- musens rörelse
+                    background.scrollTo(0, x)
+                }
+            }
+
         }, 1000)
+
     }, 2000)
+
 }, 5000)
-
-
-//lägga till loading animation (såndär hjulgrej)
